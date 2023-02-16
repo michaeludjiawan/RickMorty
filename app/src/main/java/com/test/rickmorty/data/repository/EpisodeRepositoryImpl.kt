9d47e433b334b14bb10757bee20516c1cedb9063
michaeludjiawan <michael.udjiawan@gmail.com>
2023-02-17 03:13:06 +0700

    Wrap all repository call in Dispatcher IO

--- app/src/main/java/com/test/rickmorty/data/repository/EpisodeRepositoryImpl.kt
+++ app/src/main/java/com/test/rickmorty/data/repository/EpisodeRepositoryImpl.kt
@@ -3,13 +3,15 @@ package com.test.rickmorty.data.repository
 import com.test.rickmorty.data.api.ApiService
 import com.test.rickmorty.data.model.Episode
 import com.test.rickmorty.util.NetworkUtil.safeApiCall
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class EpisodeRepositoryImpl(
     private val apiService: ApiService
 ) : EpisodeRepository {
     override suspend fun getEpisodes(ids: IntArray): Result<List<Episode>> {
-        val episodes = safeApiCall { apiService.getEpisodes(ids.contentToString()) }
-
-        return episodes
+        return withContext(Dispatchers.IO) {
+            safeApiCall { apiService.getEpisodes(ids.contentToString()) }
+        }
     }
 }
--- app/src/main/java/com/test/rickmorty/data/repository/LocationRepositoryImpl.kt
+++ app/src/main/java/com/test/rickmorty/data/repository/LocationRepositoryImpl.kt
@@ -3,11 +3,15 @@ package com.test.rickmorty.data.repository
 import com.test.rickmorty.data.api.ApiService
 import com.test.rickmorty.data.model.Location
 import com.test.rickmorty.util.NetworkUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class LocationRepositoryImpl(
     private val apiService: ApiService
 ) : LocationRepository {
     override suspend fun getLocations(ids: IntArray): Result<List<Location>> {
-        return NetworkUtil.safeApiCall { apiService.getLocations(ids.contentToString()) }
+        return withContext(Dispatchers.IO) {
+            NetworkUtil.safeApiCall { apiService.getLocations(ids.contentToString()) }
+        }
     }
 }
